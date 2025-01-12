a0dd7848fc86b97fb1bf77bca8cf86a2ecc353c1
sondrp <sondrp@stud.ntnu.no>
2025-01-12 10:14:05 +0100

    make script for exercise creation

---
+++ startup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <exercise number>"
+  exit 1
+fi
+
+ex_num=$1
+folder_name="ex$ex_num"
+
+mkdir -p "$folder_name/problems"
+
+cat <<EOF > "$folder_name/Ex$ex_num.tex"
+\\documentclass{article}
+\\usepackage{graphicx}
+\\usepackage{amsmath}
+\\usepackage{amssymb}
+\\usepackage{listings}
+\\usepackage{xcolor}
+\\usepackage{tcolorbox}
+\\usepackage{caption}
+\\usepackage[hidelinks]{hyperref}
+\\setlength{\\parindent}{0pt}
+\\usepackage[margin=0.5in]{geometry}
+
+\\input{../macros.tex}
+
+\\begin{document}
+
+\\setExerciseNumber{$ex_num}
+
+\\input{problems/p1.tex}
+\\end{document}
+EOF
+
+cat <<EOF > "$folder_name/problems/p1.tex"
+%%% QUESTION %%%
+
+\begin{problem}{}
+
+\end{problem}
+
+%%% SOLUTION %%%
+EOF
